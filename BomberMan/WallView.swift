ac2cefad88ad10b460e77b7d03cd2894d08921bb
Kristina Del Rio Albrechet <kalbrechet@gmail.com>
2017-08-25 16:04:37 +0300

    Added wall tile

---
+++ BomberMan/WallView.swift
@@ -0,0 +1,21 @@
+//
+//  WallView.swift
+//  BomberMan
+//
+//  Created by Alejandro Del Rio Albrechet on 8/25/17.
+//  Copyright © 2017 Lv-249 iOS. All rights reserved.
+//
+
+import UIKit
+
+class WallView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

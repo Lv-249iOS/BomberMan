043bd7a9ef5f9704ee23c7548b0fa8611be25c76
yevhenRoman <yevhen_54@hotmail.com>
2017-08-28 13:12:23 +0300

    Fixes Connection class

--- BomberMan/TransitionData.swift
+++ BomberMan/TransitionData.swift
@@ -10,26 +10,43 @@ import UIKit
 
 class TransitionData: UIViewController {
 
+    @IBOutlet weak var connectionsLabel: UILabel!
+    
+    let manager = ConnectionServiceManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        manager.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    ///In this function you should write the processing of data received from another player
+    func change(playerData : String) {
+        /*
+         Processing of player data received from another player
+         */
     }
-    */
-
+    
+    /*
+        For sending data you should write in IBAction next lines:
+        manager.sendData(playerData: "There must be string with playerData")
+     */
+    
 }
+
+extension TransitionData : ConnectionServiceManagerDelegate {
+    
+    func connectedDevicesChanged(manager: ConnectionServiceManager, connectedDevices: [String]) {
+        OperationQueue.main.addOperation {
+            self.connectionsLabel.text = "Connections: \(connectedDevices)"
+        }
+    }
+    
+    func dataChanged(manager: ConnectionServiceManager, playerData: String) {
+        OperationQueue.main.addOperation {
+            print(playerData) //print
+            self.change(playerData: playerData)
+            NSLog("%@", "Unknown player data value received: \(playerData)")
+        }
+    }
+}
+

1d562956b797dbb507894ed3f0e6b405d51858a6
Kristina Del Rio Albrechet <kabrechet@gmail.com>
2017-08-29 18:24:16 +0300

    Refactored WallView

--- BomberMan/WallView.swift
+++ BomberMan/WallView.swift
@@ -29,36 +29,56 @@ class WallView: UIView {
     }
     
     func addBottomBlocksLvl() {
-        addLine(from: CGPoint(x: wallRect.width, y: wallRect.height * 4 / 5), to: CGPoint(x: wallRect.width, y: wallRect.height))
-        addLine(from: CGPoint(x: wallRect.width * 3 / 5, y: wallRect.height * 4 / 5), to: CGPoint(x: wallRect.width * 3 / 5, y: wallRect.height))
-        addLine(from: CGPoint(x: 0, y: wallRect.height * 4 / 5), to: CGPoint(x: 0, y: wallRect.height))
+        addLine(from: CGPoint(x: wallRect.width, y: wallRect.height * 4 / 5),
+                to: CGPoint(x: wallRect.width, y: wallRect.height))
+        
+        addLine(from: CGPoint(x: wallRect.width * 3 / 5, y: wallRect.height * 4 / 5),
+                to: CGPoint(x: wallRect.width * 3 / 5, y: wallRect.height))
+        
+        addLine(from: CGPoint(x: 0, y: wallRect.height * 4 / 5),
+                to: CGPoint(x: 0, y: wallRect.height))
     }
     
     func addBlocksOnForthLvl() {
-        addLine(from: CGPoint(x: wallRect.width * 4 / 5, y: wallRect.height * 3 / 5), to: CGPoint(x: wallRect.width * 4 / 5, y: wallRect.height * 4 / 5))
-        addLine(from: CGPoint(x: wallRect.width / 5, y: wallRect.height * 3 / 5), to: CGPoint(x: wallRect.width / 5, y: wallRect.height * 4 / 5))
+        addLine(from: CGPoint(x: wallRect.width * 4 / 5, y: wallRect.height * 3 / 5),
+                to: CGPoint(x: wallRect.width * 4 / 5, y: wallRect.height * 4 / 5))
+        
+        addLine(from: CGPoint(x: wallRect.width / 5, y: wallRect.height * 3 / 5),
+                to: CGPoint(x: wallRect.width / 5, y: wallRect.height * 4 / 5))
     }
     
     func addBlocksOnMidLvl() {
-        addLine(from: CGPoint(x: 0, y: wallRect.height * 2 / 5), to:  CGPoint(x: 0, y: wallRect.height * 3 / 5))
-        addLine(from: CGPoint(x: wallRect.width * 2 / 5, y: wallRect.height * 2 / 5), to: CGPoint(x: wallRect.width * 2 / 5, y: wallRect.height * 3 / 5))
-        addLine(from: CGPoint(x: wallRect.width, y: wallRect.height * 2 / 5), to: CGPoint(x: wallRect.width, y: wallRect.height * 3 / 5))
+        addLine(from: CGPoint(x: 0, y: wallRect.height * 2 / 5),
+                to:  CGPoint(x: 0, y: wallRect.height * 3 / 5))
+        
+        addLine(from: CGPoint(x: wallRect.width * 2 / 5, y: wallRect.height * 2 / 5),
+                to: CGPoint(x: wallRect.width * 2 / 5, y: wallRect.height * 3 / 5))
+        
+        addLine(from: CGPoint(x: wallRect.width, y: wallRect.height * 2 / 5),
+                to: CGPoint(x: wallRect.width, y: wallRect.height * 3 / 5))
     }
     
     func addBlocksOnSecondLvl() {
-        addLine(from: CGPoint(x: wallRect.width / 5, y: wallRect.height / 5), to: CGPoint(x: wallRect.width / 5, y: wallRect.height * 2 / 5))
-        addLine(from: CGPoint(x: wallRect.width * 3 / 5, y: wallRect.height / 5), to: CGPoint(x: wallRect.width * 3 / 5, y: wallRect.height * 2 / 5))
+        addLine(from: CGPoint(x: wallRect.width / 5, y: wallRect.height / 5),
+                to: CGPoint(x: wallRect.width / 5, y: wallRect.height * 2 / 5))
+        
+        addLine(from: CGPoint(x: wallRect.width * 3 / 5, y: wallRect.height / 5),
+                to: CGPoint(x: wallRect.width * 3 / 5, y: wallRect.height * 2 / 5))
     }
     
     func addBlocksOnTopLvl() {
         addLine(from: CGPoint(x: 0, y: 0), to: CGPoint(x: 0, y: wallRect.height / 5))
-        addLine(from: CGPoint(x: wallRect.width * 2 / 5, y: 0), to: CGPoint(x: wallRect.width * 2 / 5, y: wallRect.height / 5))
-        addLine(from: CGPoint(x: wallRect.width, y: 0), to: CGPoint(x: wallRect.width, y: wallRect.height / 5))
+        addLine(from: CGPoint(x: wallRect.width * 2 / 5, y: 0),
+                to: CGPoint(x: wallRect.width * 2 / 5, y: wallRect.height / 5))
+        
+        addLine(from: CGPoint(x: wallRect.width, y: 0),
+                to: CGPoint(x: wallRect.width, y: wallRect.height / 5))
     }
     
     func addBlocksLevels() {
         for i in 0 ..< 6 {
-            addLine(from: CGPoint(x: 0, y: wallRect.height * CGFloat(i) / 5), to:  CGPoint(x: wallRect.width, y: wallRect.height * CGFloat(i) / 5))
+            addLine(from: CGPoint(x: 0, y: wallRect.height * CGFloat(i) / 5),
+                    to:  CGPoint(x: wallRect.width, y: wallRect.height * CGFloat(i) / 5))
         }
     }
     

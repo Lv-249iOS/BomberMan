e492626fc543e79463a082b8ffc69561a56a37b6
yevhenRoman <yevhen_54@hotmail.com>
2017-08-30 15:59:41 +0300

    MultipeerConnectivity

---
+++ BomberMan/ConnectionGuide.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectionGuide.swift
+//  BomberMan
+//
+//  Created by Yevhen Roman on 8/30/17.
+//  Copyright © 2017 Lv-249 iOS. All rights reserved.
+//
+
+import Foundation
+import MultipeerConnectivity
+
+class ConnectionGuide {
+    
+    func getCountOfFoundDevices() {
+        
+    }
+}

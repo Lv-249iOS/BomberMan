4ac9a8f38d8dd5a1cf8f5a3ed4dab681cb109325
Zhanna Moskaliuk <octoberrock11@gmail.com>
2017-08-30 12:10:29 +0300

    Fixed constraints of table view

--- BomberMan/RatingController.swift
+++ BomberMan/RatingController.swift
@@ -15,9 +15,8 @@ class RatingController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-        
     }
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
@@ -26,16 +25,4 @@ class RatingController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

ed3ac5f8e334b239c7ac9bcfbed0e9c39fa42dd3
AndriiMoravskyi <andriy_moravskyi@macminissu10.training.local>
2017-08-31 15:25:56 +0300

    code refactored, add function for image animation

--- BomberMan/GameMapController.swift
+++ BomberMan/GameMapController.swift
@@ -64,106 +64,68 @@ class GameMapController: UIViewController {
         if Brain.shared.move(to: direction, player: Player()) {
             switch direction {
             case .bottom:
-                
                 let downImageArray = (1...5).map { UIImage(named: "down\($0)") ?? #imageLiteral(resourceName: "noImage")  }
-                
                 if orc.animationImages?.first != UIImage(named: "down1") && orc.animationImages?.first != nil {
-                    count = 0
-                    countAnimation = 0
-                    orc.stopAnimating()
-                    orc.animationImages = downImageArray
-                    orc.animationDuration = 0.5
-                    orc.startAnimating()
                     
-                    orc.layer.removeAllAnimations()
+                    animate(images: downImageArray)
                 }
-                count += 1
-                animateImages(images: downImageArray, x: 0, y: 50)
+                animateImagesforMove(images: downImageArray, x: 0, y: 50)
                 
             case .left:
-                
                 let leftImageArray = (1...5).map { UIImage(named: "left\($0)") ?? #imageLiteral(resourceName: "noImage") }
-                
                 if orc.animationImages?.first != UIImage(named: "left1") && orc.animationImages?.first != nil {
-                    count = 0
-                    countAnimation = 0
-                    orc.stopAnimating()
-                    orc.animationImages = leftImageArray
-                    orc.animationDuration = 0.5
-                    orc.startAnimating()
-                    
-                    orc.layer.removeAllAnimations()
+                    animate(images: leftImageArray)
                 }
-                count += 1
-                animateImages(images: leftImageArray, x: -50, y: 0)
+                animateImagesforMove(images: leftImageArray, x: -50, y: 0)
                 
             case .right:
-                
-                
                 let rightImageArray = (1...5).map { UIImage(named: "right\($0)") ?? #imageLiteral(resourceName: "noImage") }
-                
                 if orc.animationImages?.first != UIImage(named: "right1") && orc.animationImages?.first != nil{
-                    count = 0
-                    countAnimation = 0
-                    orc.stopAnimating()
-                    orc.animationImages = rightImageArray
-                    orc.animationDuration = 0.5
-                    orc.startAnimating()
                     
-                    orc.layer.removeAllAnimations()
+                    animate(images: rightImageArray)
                 }
-            count += 1
-                animateImages(images: rightImageArray, x: 50, y: 0)
+                animateImagesforMove(images: rightImageArray, x: 50, y: 0)
                 
             case .top:
-                
                 let upImageArray = (1...5).map { UIImage(named: "up\($0)") ?? #imageLiteral(resourceName: "noImage") }
-                
                 if orc.animationImages?.first != UIImage(named: "up1") && orc.animationImages?.first != nil {
-                    count = 0
-                    countAnimation = 0
-                    orc.stopAnimating()
-                    orc.animationImages = upImageArray
-                    orc.animationDuration = 0.5
-                    orc.startAnimating()
                     
-                    orc.layer.removeAllAnimations()
+                    animate(images: upImageArray)
                 }
-                count += 1
-                animateImages(images: upImageArray, x: 0, y: -50)
+                animateImagesforMove(images: upImageArray, x: 0, y: -50)
             }
+            count += 1
         }
     }
     
-    func animateImages(images:[UIImage],x:CGFloat,y:CGFloat){
+    func animateImagesforMove(images:[UIImage],x:CGFloat,y:CGFloat){
         let op  = UIViewAnimationOptions.beginFromCurrentState
-        
         UIView.animate(withDuration: 0.5, delay: 0, options: op, animations: {[weak self] in
-            
             if !(self?.orc.isAnimating)! {
-                
                 self?.orc.animationImages = images
                 self?.orc.animationDuration = 0.5
                 self?.orc.startAnimating()
-            
             }
-            
             self?.orc.transform = (self?.orc.transform.translatedBy(x: x, y: y))!
             }, completion: { [weak self] finished in
-
                 if finished {
-
                     self?.countAnimation += 1
-                    
                     if self?.count == self?.countAnimation {
-                        
-                    self?.orc.stopAnimating()
-                    self?.countAnimation = 0
-                    self?.count = 0
-                        
+                        self?.orc.stopAnimating()
+                        self?.countAnimation = 0
+                        self?.count = 0
                     }
-                    
                 }
         })
+    }
+    
+    func animate(images:[UIImage]) {
+        count = 0
+        countAnimation = 0
+        orc.stopAnimating()
+        orc.animationImages = images
+        orc.animationDuration = 0.5
+        orc.startAnimating()
+        orc.layer.removeAllAnimations()
     }
 }

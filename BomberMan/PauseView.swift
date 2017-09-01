0198b5878908cae31126cabe4c919e5769154a1c
Kristina Del Rio Albrechet <kabrechet@gmail.com>
2017-09-01 14:45:03 +0300

    Added pause xib

--- BomberMan/PauseView.swift
+++ BomberMan/PauseView.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class PauseView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var pauseButton: UIButton!
+    
+    var onPauseButtTap: (()->())?
+    
+    @IBAction func onPauseTap(_ sender: UIButton) {
+        onPauseButtTap?()
     }
-    */
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed("PauseView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
 }

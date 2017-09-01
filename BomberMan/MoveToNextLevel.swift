6a17656c49d1694a24ec1afda6b80517f0b95396
Kristina Del Rio Albrechet <kabrechet@gmail.com>
2017-09-01 14:44:28 +0300

    Added move on xib

---
+++ BomberMan/MoveToNextLevel.swift
@@ -0,0 +1,48 @@
+//
+//  MoveToNextLevel.swift
+//  BomberMan
+//
+//  Created by Alejandro Del Rio Albrechet on 9/1/17.
+//  Copyright © 2017 Lv-249 iOS. All rights reserved.
+//
+
+import UIKit
+
+class MoveToNextLevel: UIView {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var buttonsStack: UIStackView!
+    @IBOutlet weak var replayButton: UIButton!
+    @IBOutlet weak var moveOnButton: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    var onRepeatButtTap: (()->())?
+    var onMoveOnButtTap: (()->())?
+
+    @IBAction func onRepeatTap(_ sender: UIButton) {
+        onRepeatButtTap?()
+    }
+    
+    @IBAction func onContinueTap(_ sender: UIButton) {
+        onMoveOnButtTap?()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed("MoveToNextLevel", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+}
---
+++ BomberMan/PauseView.swift
@@ -0,0 +1,21 @@
+//
+//  PauseView.swift
+//  BomberMan
+//
+//  Created by Alejandro Del Rio Albrechet on 9/1/17.
+//  Copyright © 2017 Lv-249 iOS. All rights reserved.
+//
+
+import UIKit
+
+class PauseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

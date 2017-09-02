97df07582a2492ad80c5354dc452276aafcade86
Kristina Del Rio Albrechet <kabrechet@gmail.com>
2017-09-02 18:42:12 +0300

    Added comments for wall view tile

--- BomberMan/CustomTiles/WallView.swift
+++ BomberMan/CustomTiles/WallView.swift
@@ -10,6 +10,7 @@ import UIKit
 
 @IBDesignable
 class WallView: UIView {
+    
     let wallPath = UIBezierPath()
     var wallRect: CGRect!
     
@@ -28,6 +29,7 @@ class WallView: UIView {
         wallPath.stroke()
     }
     
+    // Adds bottom level of brick wall
     func addBottomBlocksLvl() {
         addLine(from: CGPoint(x: wallRect.width, y: wallRect.height * 4 / 5),
                 to: CGPoint(x: wallRect.width, y: wallRect.height))
@@ -39,6 +41,7 @@ class WallView: UIView {
                 to: CGPoint(x: 0, y: wallRect.height))
     }
     
+    // Adds level upper bottom of brick wall
     func addBlocksOnForthLvl() {
         addLine(from: CGPoint(x: wallRect.width * 4 / 5, y: wallRect.height * 3 / 5),
                 to: CGPoint(x: wallRect.width * 4 / 5, y: wallRect.height * 4 / 5))
@@ -47,6 +50,7 @@ class WallView: UIView {
                 to: CGPoint(x: wallRect.width / 5, y: wallRect.height * 4 / 5))
     }
     
+    // Adds middle layer of brick wall
     func addBlocksOnMidLvl() {
         addLine(from: CGPoint(x: 0, y: wallRect.height * 2 / 5),
                 to:  CGPoint(x: 0, y: wallRect.height * 3 / 5))
@@ -58,6 +62,7 @@ class WallView: UIView {
                 to: CGPoint(x: wallRect.width, y: wallRect.height * 3 / 5))
     }
     
+    // Adds level under top level of brick wall
     func addBlocksOnSecondLvl() {
         addLine(from: CGPoint(x: wallRect.width / 5, y: wallRect.height / 5),
                 to: CGPoint(x: wallRect.width / 5, y: wallRect.height * 2 / 5))
@@ -66,6 +71,7 @@ class WallView: UIView {
                 to: CGPoint(x: wallRect.width * 3 / 5, y: wallRect.height * 2 / 5))
     }
     
+    // Adds top level of brick wall
     func addBlocksOnTopLvl() {
         addLine(from: CGPoint(x: 0, y: 0), to: CGPoint(x: 0, y: wallRect.height / 5))
         addLine(from: CGPoint(x: wallRect.width * 2 / 5, y: 0),
@@ -75,6 +81,7 @@ class WallView: UIView {
                 to: CGPoint(x: wallRect.width, y: wallRect.height / 5))
     }
     
+    // Adds horizontal lines for every level of bricks
     func addBlocksLevels() {
         for i in 0 ..< 6 {
             addLine(from: CGPoint(x: 0, y: wallRect.height * CGFloat(i) / 5),
@@ -82,6 +89,7 @@ class WallView: UIView {
         }
     }
     
+    // Adds line between two points
     func addLine(from startPoint: CGPoint, to endPoint: CGPoint) {
         wallPath.move(to: startPoint)
         wallPath.addLine(to: endPoint)

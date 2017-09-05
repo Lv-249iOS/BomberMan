e3c9b493baa3b34a10a536fdcdcf9899daca8eb8
AndriiMoravskyi <andriy_moravskyi@macminissu10.training.local>
2017-09-05 18:22:42 +0300

    Modified levels

--- BomberMan/Brain/Levels.swift
+++ BomberMan/Brain/Levels.swift
@@ -11,15 +11,26 @@ import Foundation
 class Levels {
     
     enum Level: String {
-        case one = "WWWWWWWWWWW  0     WW        WW        WW  B BBB WW  B B   WW  BBBBB WW    B B WWM BBBMB WWWWWWWWWWW"
-        case two = "WWWWWWWWWWWWWWWW MWBW MBBBMWWWW             WW   BBBBB  WW WW  BBBBBBB    WW  BBBBB B    WW   BBBBB     WW  M BBB      WW     B M  0  WW     BBB     WW            MWWWWWWWWWWWWWWWW"
-        case three = "WWWWWWWWWWWWWWWW 0           WW    BBWBB    WW   BBBWBBB   WW   BBBWBBB   WW    BBBBB    WW      B      WW     BBB     WW      B      WW  BBB WWWWW BWW          BWBWWWWWWWWWWWWWWWW"
-        case four = "WWWWWWWWWWWWWWWW0  BBBBBBBB  WW WBW W W W W WW BB BBBB BBBBWWBW WBWBWBWBWBWWBBBBBB BB   BWWBWBWBWBWBWBW WWBBB B BBB  BBWWBWBWBW WBWBWBWW BBBBBBBBBBBBWW WBWBWBWBWBW WW  BBBBB BBB  WWWWWWWWWWWWWWWW"
-        case five = "WWWWWWWWWWWWWWWWBB  B  BBB WBWW   BBW WBWBWBWWWWWBWBW  WBW WW  B  BB  BBBWWWB  BBB   W 0 WW             WW   BBB W BBB WW WWW    WWB  WWW BWBW  BWB  WW     B BB    WWWWWWWWWWWWWWWW"
-        case six = "WWWWWWWWWWWWWWWWBB  B BB BBB WWBWBWBW WBWBWBWWBBBBBB  BBBBBWWBWBWBWBWBWBWBWW  B 0    BB  WW WBW W W W WBWW  B        B WW WBWBW WBW WBWWBBBB B B BBB WW W WBWBW W WBWWBB BBBB B BB WWWWWWWWWWWWWWWW"
-        case seven = "WWWWWWWWWWWWWWWW   B B BB B 0WW WBW WBWBWBW WWB B   BBBB BBWWBW WBWBWBWBWBWWB B BBBBB  BBWWBWBWBWBW  WBWWW BB BB BBBB  WW WBW W WBW W WW BB  B   B BBWW WBWBW WBW W WW   B     BB  WWWWWWWWWWWWWWWW"
-        case eight = "WWWWWWWWWWWWWWWW    BB BB B 0WW BWB B B B B WWWWWW W  WW W WW BWBWBWBWB BWWWW  WW  WW WWWWW BWB BWBWBWBWWWWB B BWBWB B WW WWW WW   WWWWW BWB B B B B WW   W  WW     WWWWWWWWWWWWWWWW"
-        case nine = "WWWWWWWWWWWWWWWW   BBBB B  WWWW WBWWWWWWWWW WW WBBW 0  BWW WW W         WWWW BW B      WWWW WWWWWWWBWWW WWB WBW WBWBWBWWWW   BBB BBBB WWBBB  WBW BB  WWWWBBWWW  B  WWWWWWWWWWWWWWWWW"
+        case one = "WWWWWWWWWWW  0     WW        WW        WW  B BBB WW  B B   WW  BBDBB WW    B B WWM BBBMB WWWWWWWWWWW"
+        case two = "WWWWWWWWWWWWWWWW MWBW MBBBMWWWW             WW   BUBBB  WW WW  BBBBBBB    WW  BBDBBUB    WW   BBBBB     WW  M BBB      WW     BM   0  WW     BBB     WW            MWWWWWWWWWWWWWWWW"
+        
+        case three = "WWWWWWWWWWWWWWWW 0          MWW    BBWBB    WW   BBBWBBB   WW   BBBWBDM   WW    BBBBB    WW M    B      WW     BUB     WW      BM     WW  BUB WWWWW BWWM         BWBWWWWWWWWWWWWWWWW"
+        
+        case four = "WWWWWWWWWWWWWWWW0 BUBBBBBBB  WW WBW W W W W WW BB MBBB BBBBWWBW WBWBWBWBWBWWBBBBBB BB M BWWBWBWBWDWBWBW WWBBM B BBM  BBWWBWBWBW WBWBWBWW BBBBBBB MBBBWW WBWBWBWBWBW WW MBBBBB UBBM WWWWWWWWWWWWWWWW"
+        
+        case five = "WWWWWWWWWWWWWWWWBB  B  BBB WBWW   UBW WMWBWBWWWWWBWBW  WBW WW  B MBB  BWBWWWB  BDB   W 0 WW             WW   UBB W BWB WW WWWM   WWB  WWW BWMW  BWB MWW  M  B BB    WWWWWWWWWWWWWWWW"
+        
+        
+        case six = "WWWWWWWWWWWWWWWWBB  B BB BBB WWBWMWMW WBWBWBWWBBBB B  BBBBBWWBWBW WBWUWBWBWW  B 0    BB MWW WBW W W W WBWW  M        B WW WBWBW WBW WBWWBBBB U B BMB WW W WBWMW W WBWWBB DBBB B BB WWWWWWWWWWWWWWWW"
+        
+        case seven = "WWWWWWWWWWWWWWWW   M B B  U 0WW WBW WBWBWBW WWB B   BBBB BBWWBW WBWBWBWBWBWWB B MBBBBM BBWWBWBWDWBW  WBWWW BB BB BBBB  WW WBW W WBW W WW BBM B   B BBWW WBWBW WBW W WW   B  M  BB  WWWWWWWWWWWWWWWW"
+        
+        case eight = "WWWWWWWWWWWWWWWW    BU BWBB 0WW BWB B B BWB WWMWWW W  WWMW WW BWBWUWBWM B WWW MWW  WW WWWWW BWB BWBWBWBMWWWB BMBMBWB B WW WUW WW   WWWWWMBWB M BDB B WW   W  WW   M WWWWWWWWWWWWWWWW"
+        
+        case nine = "WWWWWWWWWWWWWWWW   MBUB     WWW WBWWWB WWW  WW WBMW 0  MWW WWMW  M      W WW BW B      W WW WWWWMWWMWWW WWB WMW WBWBWBWWWW   BBB BBBB WWBBUM WDW BBM WWWWBBMWW  B  WWWWWWWWWWWWWWWWW"
+        
+        
+        
     }
     
     let levels = [
@@ -46,103 +57,121 @@ class Levels {
 
 /*
  case one = "WWWWWWWWWW
-             W  0     W
-             W        W
-             W        W
-             W  B BBB W
-             W  B B   W
-             W  BBDBB W
-             W    B B W
-             WM BBBMB W
-             WWWWWWWWWW"
+ W  0     W
+ W        W
+ W        W
+ W  B BBB W
+ W  B B   W
+ W  BBDBB W
+ W    B B W
+ WM BBBMB W
+ WWWWWWWWWW"
  
  case two = "WWWWWWWWWWWWWWW
-             W MWBW MBBBMWWW
-             W             W
-             W   BUBBB  WW W
-             W  BBBBBBB    W
-             W  BBDBBUB    W
-             W   BBBBB     W
-             W  M BBB      W
-             W     B M  0  W
-             W     BBB     W
-             W            MW
-             WWWWWWWWWWWWWWW"
+ W MWBW MBBBMWWW
+ W             W
+ W   BUBBB  WW W
+ W  BBBBBBB    W
+ W  BBDBBUB    W
+ W   BBBBB     W
+ W  M BBB      W
+ W     BM   0  W
+ W     BBB     W
+ W            MW
+ WWWWWWWWWWWWWWW"
  
  case three = "WWWWWWWWWWWWWWW
-               W 0          MW
-               W M  BBWBB    W
-               W   BBBWBBB   W
-               W   BBBWBDM   W
-               W    BBBBB    W
-               W      B      W
-               W     BUB     W
-               W      BM     W
-               W  BUB WWWWW BW
-               WM         BWBW
-               WWWWWWWWWWWWWWW"
-
+ W 0          MW
+ W    BBWBB    W
+ W   BBBWBBB   W
+ W   BBBWBDM   W
+ W    BBBBB    W
+ W M    B      W
+ W     BUB     W
+ W      BM     W
+ W  BUB WWWWW BW
+ WM         BWBW
+ WWWWWWWWWWWWWWW"
+ 
  case four = "WWWWWWWWWWWWWWW
-              W0 MUBBBBBBB  W
-              W WBW W W W W W
-              W BB BBBB BBBBW
-              WBW WBWBWBWBWBW
-              WBBBBBB BB M BW
-              WBWBWBWDWBWBW W
-              WBBB B BBB  BBW
-              WBWBWBW WBWBWBW
-              W BBBBBBBBBBBBW
-              W WBWBWBWBWBW W
-              W MBBBBB UBBM W
-              WWWWWWWWWWWWWWW"
+ W0 BUBBBBBBB  W
+ W WBW W W W W W
+ W BB MBBB BBBBW
+ WBW WBWBWBWBWBW
+ WBBBBBB BB M BW
+ WBWBWBWDWBWBW W
+ WBBM B BBM  BBW
+ WBWBWBW WBWBWBW
+ W BBBBBBB MBBBW
+ W WBWBWBWBWBW W
+ W MBBBBB UBBM W
+ WWWWWWWWWWWWWWW"
  
  case five = "WWWWWWWWWWWWWWW
-              WBB  B  BBB WBW
-              W   UBW WMWBWBW
-              WWWWBWBW  WBW W
-              W  B MBB  BMBWW
-              WB  BDB   W 0 W
-              W            MW
-              W   UBB W BMB W
-              W WWWM   WWB  W
-              WW BWMW  BWB  W
-              W  M  B BB    W
-              WWWWWWWWWWWWWWW"
+ WBB  B  BBB WBW
+ W   UBW WMWBWBW
+ WWWWBWBW  WBW W
+ W  B MBB  BWBWW
+ WB  BDB   W 0 W
+ W             W
+ W   UBB W BWB W
+ W WWWM   WWB  W
+ WW BWMW  BWB MW
+ W  M  B BB    W
+ WWWWWWWWWWWWWWW"
  
  case six = "WWWWWWWWWWWWWWW
-             WBB  B BB BBB W
-             WBWMWBW WBWBWBW
-             WBBBBBB  BBBBBW
-             WBWBWMWBWUWBWBW
-             W  B 0    BB  W
-             W WBW W W W WBW
-             W  M        B W
-             W WBWBW WBW WBW
-             WBBBB U B BMB W
-             W W WBWMW W WBW
-             WBB BBBB B BB W
-             WWWWWWWWWWWWWWW"
-
+ WBB  B BB BBB W
+ WBWMWMW WBWBWBW
+ WBBBB B  BBBBBW
+ WBWBW WBWUWBWBW
+ W  B 0    BB MW
+ W WBW W W W WBW
+ W  M        B W
+ W WBWBW WBW WBW
+ WBBBB U B BMB W
+ W W WBWMW W WBW
+ WBB DBBB B BB W
+ WWWWWWWWWWWWWWW"
+ 
  case seven = "WWWWWWWWWWWWWWW
-               W   M B BB U 0W
-               W WBW WBWBWBWMW
-               WB B   BBBB BBW
-               WBW WBWBWBWBWBW
-               WB B MBBBB  BBW
-               WBWBWBWBW  WBWW
-               W BB BB BBBB  W
-               W WBW W WBW W W
-               W BB  B   B BBW
-               W WBWBW WBW W W
-               W   B     BB  W
-               WWWWWWWWWWWWWWW"
+ W   M B B  U 0W
+ W WBW WBWBWBW W
+ WB B   BBBB BBW
+ WBW WBWBWBWBWBW
+ WB B MBBBBM BBW
+ WBWBWDWBW  WBWW
+ W BB BB BBBB  W
+ W WBW W WBW W W
+ W BBM B   B BBW
+ W WBWBW WBW W W
+ W   B  M  BB  W
+ WWWWWWWWWWWWWWW"
  
- case eight = "WWWWWWWWWWWWWWWW    BB BB B 0WW BWB B B B B WWWWWW W  WW W WW BWBWBWBWB BWWWW  WW  WW WWWWW BWB BWBWBWBWWWWB B BWBWB B WW WWW WW   WWWWW BWB B B B B WW   W  WW     WWWWWWWWWWWWWWWW"
+ case eight = "WWWWWWWWWWWWWWW
+ W    BU BWBB 0W
+ W BWB B B BWB W
+ WMWWW W  WWMW W
+ W BWBWUWBWM B W
+ WW MWW  WW WWWW
+ W BWB BWBWBWBMW
+ WWB BMBMBWB B W
+ W WUW WW   WWWW
+ WMBWB M BDB B W
+ W   W  WW   M W
+ WWWWWWWWWWWWWWW"
  
- case nine = "WWWWWWWWWWWWWWWW   BBBB B  WWWW WBWWWWWWWWW WW WBBW 0  BWW WW W         WWWW BW B      WWWW WWWWWWWBWWW WWB WBW WBWBWBWWWW   BBB BBBB WWBBB  WBW BB  WWWWBBWWW  B  WWWWWWWWWWWWWWWWW"
-
- 
- 
- 
+ case nine = "WWWWWWWWWWWWWWW
+ W   MBUB     WW
+ W WBWWWB WWW  W
+ W WBMW 0  MWW W
+ WMW  M      W W
+ W BW B      W W
+ W WWWWMWWMWWW W
+ WB WMW WBWBWBWW
+ WW   BBB BBBB W
+ WBBUM WDW BBM W
+ WWWBBMWW  B  WW
+ WWWWWWWWWWWWWWW"
  
  */

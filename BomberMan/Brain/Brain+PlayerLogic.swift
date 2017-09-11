1e2f0807ae3a7f33fabc0e19950538e23ce048ed
Yaroslav Luchyt <uter8764@gmail.com>
2017-09-11 15:20:03 +0300

    modified move function

--- BomberMan/Brain/Brain+PlayerLogic.swift
+++ BomberMan/Brain/Brain+PlayerLogic.swift
@@ -11,82 +11,76 @@ import Foundation
 extension Brain {
     
     func move(to direction: Direction, player: inout Player) {
+        var directionPosition: Int
+        var shouldRedraw = false
         
-        if let playerPosition = scene.data.characters.index(of: player.markForScene) ?? scene.data.characters.index(of: "Q") {
-            var directionPosition: Int
-            var canGo = true
-            var shouldRedraw = false
-            
-            switch direction {
-            case .bottom: directionPosition = player.position + scene.width
-            case .left: directionPosition = player.position - 1
-            case .right: directionPosition = player.position + 1
-            case .top: directionPosition = player.position - scene.width
+        switch direction {
+        case .bottom: directionPosition = player.position + scene.width
+        case .left: directionPosition = player.position - 1
+        case .right: directionPosition = player.position + 1
+        case .top: directionPosition = player.position - scene.width
+        }
+        if playerCanGo(to: directionPosition) {
+            let last = tiles[directionPosition].last ?? " "
+            switch last {
+            case "F":
+                tiles[player.position].removeLast()
+                if let intValue = Int(player.markForScene.description) {
+                    move?(direction, intValue)
+                    killHero?(intValue)
+                    gameEnd?(false)
+                    score -= 1000
+                    if score < 0 {
+                        score = 0
+                    }
+                    refreshScore?(score)
+                    return
+                }
+            case "U":
+                if let index = getUpgradeIndex(atPosition: directionPosition) {
+                    switch upgrades[index].type {
+                    case .anotherBomb:
+                        player.minesCount += 1
+                    case .strongerBomb:
+                        player.explosionPower += 1
+                    }
+                    upgrades.remove(at: index)
+                    score += 100
+                    refreshScore?(score)
+                    shouldRedraw = true
+                }
+            case "D":
+                if let intValue = Int(player.markForScene.description) {
+                    score += 500
+                    refreshScore?(score)
+                    move?(direction, intValue)
+                    gameEnd?(true)
+                    return
+                }
+            case "M":
+                tiles[player.position].removeLast()
+                if let intValue = Int(player.markForScene.description) {
+                    move?(direction, intValue)
+                    killHero?(intValue)
+                    gameEnd?(false)
+                    score -= 1000
+                    if score < 0 {
+                        score = 0
+                    }
+                    refreshScore?(score)
+                    return
+                }
+            default:
+                break
             }
-            if playerCanGo(to: directionPosition) {
-                let last = tiles[directionPosition].last ?? " "
-                switch last {
-                case "F":
-                    tiles[player.position].removeLast()
-                    if let intValue = Int(player.markForScene.description) {
-                        move?(direction, intValue)
-                        killHero?(intValue)
-                        gameEnd?(false)
-                        score -= 1000
-                        if score < 0 {
-                            score = 0
-                        }
-                        refreshScore?(score)
-                        return
-                    }
-                case "U":
-                    if let index = getUpgradeIndex(atPosition: directionPosition) {
-                        switch upgrades[index].type {
-                        case .anotherBomb:
-                            player.minesCount += 1
-                        case .strongerBomb:
-                            player.explosionPower += 1
-                        }
-                        upgrades.remove(at: index)
-                        score += 100
-                        refreshScore?(score)
-                        shouldRedraw = true
-                    }
-                case "D":
-                    if let intValue = Int(player.markForScene.description) {
-                        score += 500
-                        refreshScore?(score)
-                        move?(direction, intValue)
-                        gameEnd?(true)
-                        return
-                    }
-                case "M":
-                    tiles[player.position].removeLast()
-                    if let intValue = Int(player.markForScene.description) {
-                        move?(direction, intValue)
-                        killHero?(intValue)
-                        gameEnd?(false)
-                        score -= 1000
-                        if score < 0 {
-                            score = 0
-                        }
-                        refreshScore?(score)
-                        return
-                    }
-                default:
-                    break
-                }
-                
-                if canGo {
-                    tiles[player.position].removeLast()
-                    tiles[directionPosition].append(player.markForScene)
-                    if let intValue = Int(player.markForScene.description) {
-                        move?(direction, intValue)
-                    }
-                    if shouldRedraw {
-                        redrawScene?()
-                    }
-                }
+            tiles[player.position].removeLast()
+            tiles[directionPosition].append(player.markForScene)
+            player.position = directionPosition
+            if let intValue = Int(player.markForScene.description) {
+                move?(direction, intValue)
+            }
+            if shouldRedraw {
+                redrawScene?()
             }
         }
     }
@@ -108,10 +102,11 @@ extension Brain {
         return true
     }
     
-    func plantBomb(player: Player) {
+    func plantBomb(player: inout Player) {
         if player.minesCount > player.plantedMines, canFitBomb(at: player.position)  {
             tiles[player.position].insert("X", at: 0)
-            startBombTimer(at: playerPosition, player: player)
+            player.plantedMines += 1
+            startBombTimer(withOptionsOfPlayer: player)
             
             if let intValue = Int(player.markForScene.description) {
                 plantBomb?(intValue)

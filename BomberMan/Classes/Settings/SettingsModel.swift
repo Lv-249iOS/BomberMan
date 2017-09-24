e5fc74276cd6f2527a731adf93b3c7b356e4e171
moskaliukzhanna <octoberrock11@gmail.com>
2017-09-24 13:46:22 +0300

    Revert "Zhanna development"

--- BomberMan/Classes/Settings/SettingsModel.swift
+++ BomberMan/Classes/Settings/SettingsModel.swift
@@ -12,33 +12,15 @@ import UIKit
 class SettingsModel {
     
     static let shared = SettingsModel()
-    
+
     enum PathForKey: String {
-<<<<<<< HEAD
-        case currentPageOfImage = "currentPageOfImage"
-        case controlMode = "controlMode"
-=======
         case currentCharacterImage = "currentCharacterImage"
         case currentPlayMode = "currentPlayMode"
->>>>>>> master
     }
     var characterImagesArray = [ #imageLiteral(resourceName: "bom1"),#imageLiteral(resourceName: "bom2"),#imageLiteral(resourceName: "bom3"),#imageLiteral(resourceName: "bom4")]
     var gameModeArray = ["Only buttons","Buttons and gestures"]
     
-<<<<<<< HEAD
-    var gameModeArray = [ //Need add images for game play
-        #imageLiteral(resourceName: "bom1"),
-        #imageLiteral(resourceName: "bom2"),
-        #imageLiteral(resourceName: "bom3"),
-        #imageLiteral(resourceName: "bom4")
-    ]
-    
-    var pickerModeArray = ["Buttons", "Buttons and gestures"]
-    
-    var currentPage: Int {
-=======
     var currentCharacterImage: Int {
->>>>>>> master
         get {
             if let value = UserDefaults.standard.value(forKey: PathForKey.currentCharacterImage.rawValue) as? Int {
                 return value
@@ -63,19 +45,4 @@ class SettingsModel {
         }
     }
     
-<<<<<<< HEAD
-    var currentMode: String {
-        get {
-            if let value = UserDefaults.standard.string(forKey: PathForKey.controlMode.rawValue) {
-                return value
-            } else {
-                return "Buttons"
-            }
-        }
-        set {
-            UserDefaults.standard.set(newValue, forKey: PathForKey.controlMode.rawValue)
-        }
-    }
-=======
->>>>>>> master
 }
